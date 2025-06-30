36a40afa61defb2ea4fc0fb225e1e58ec3fce553
abdul <abdul.tanveer@gmail.com>
2025-06-30 10:55:16 +0530

    snackbar, get the text et and show it in tv

--- app/src/main/java/com/example/boschyd/MainActivity.kt
+++ app/src/main/java/com/example/boschyd/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.boschyd
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //inflation
 
+    }
+
+    fun handleClick(view: View) {
+        var clMain:ConstraintLayout = findViewById(R.id.maincl)
+        Snackbar.make(clMain,"button clicked",Snackbar.LENGTH_SHORT).show()
+        var nameEt:EditText = findViewById(R.id.etName) //taking handle
+        var name:String = nameEt.text.toString()
+        val resultTv:TextView = findViewById(R.id.tvResult)
+        resultTv.setText(name)
     }
 }
