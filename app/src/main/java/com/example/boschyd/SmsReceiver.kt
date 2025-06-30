57407c30677658a5117142f3d34b59fe66ed5db3
abdul <abdul.tanveer@gmail.com>
2025-06-30 16:42:09 +0530

    sms broadcast receiver - phno  n message

--- app/src/main/java/com/example/boschyd/SmsReceiver.kt
+++ app/src/main/java/com/example/boschyd/SmsReceiver.kt
@@ -3,12 +3,32 @@ package com.example.boschyd
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.Telephony
 import android.util.Log
 
 class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        Log.i(TAG,"abdul u have an sms")
+        Log.i(TAG, "abdul u have an sms")
+
+        val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+        for (message in smsMessages) {
+            val sender = message.displayOriginatingAddress
+            val messageBody = message.messageBody
+
+            Log.d("SmsReceiver", "Sender: $sender")
+            Log.d("SmsReceiver", "Message body: $messageBody")
+
+//            if (sender == "+43....." || messageBody.contains("Einsatzgebiet")) {
+//                Log.d("SmsReceiver", "Message received from: $sender")
+//                Log.d("SmsReceiver", "Message body: $messageBody")
+//
+//                val launchIntent = Intent(context, CalendarActivity::class.java)
+//                launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//                launchIntent.putExtra("sms_body", messageBody)
+//                context.startActivity(launchIntent)
+//            }
+        }
     }
 
     companion object{
