cf65f136a5bb56f339f017fa514b9520780809c4
abdul <abdul.tanveer@gmail.com>
2025-07-01 11:11:43 +0530

    sms inbox content provider

--- app/src/main/java/com/example/boschyd/CalendarActivity.kt
+++ app/src/main/java/com/example/boschyd/CalendarActivity.kt
@@ -1,16 +1,28 @@
 package com.example.boschyd
 
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class CalendarActivity : AppCompatActivity() {
+    lateinit var cpList:ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_calendar)
+        cpList = findViewById(R.id.listView) //socket
 
+        val uriSms = Uri.parse("content://sms/inbox")
+        val dataCursor: Cursor = getContentResolver().query(uriSms, null, null, null, null)!!
+        var from = arrayOf("address","body")
+        var to = intArrayOf(android.R.id.text1,android.R.id.text2)
+
+        var adapter:SimpleCursorAdapter = SimpleCursorAdapter(this,
+            android.R.layout.simple_list_item_2,dataCursor,from,to,1)
+        cpList.adapter = adapter
     }
 }
